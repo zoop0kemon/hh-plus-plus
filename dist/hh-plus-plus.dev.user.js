ce81a25a8b6829d0ef1ac84320ef26466335987b
zoop0kemon <90122252+zoop0kemon@users.noreply.github.com>
2023-05-01 17:00:16 -0700

    Updating patrons list for May

--- dist/hh-plus-plus.dev.user.js
+++ dist/hh-plus-plus.dev.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Hentai Heroes++ BDSM version
 // @description     Adding things here and there in the Hentai Heroes game. Also supports HHCore-based games such as GH and CxH.
-// @version         1.34.2
+// @version         1.34.3
 // @match           https://*.hentaiheroes.com/*
 // @match           https://nutaku.haremheroes.com/*
 // @match           https://*.gayharem.com/*
@@ -3409,7 +3409,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /***/ ((module) => {
 
 "use strict";
-eval("module.exports = JSON.parse('[{\"name\":\"Safi\",\"tier\":\"gold\",\"flairs\":[{\"game\":\"HH\",\"platform\":\".com\",\"id\":\"5248781\"}]},{\"name\":\"haven4ever\",\"tier\":\"gold\",\"flairs\":[{\"game\":\"GH\",\"platform\":\".com\",\"id\":\"26313\"}]},{\"name\":\"Darkyz\",\"tier\":\"silver\",\"flairs\":[{\"game\":\"HH\",\"platform\":\".com\",\"id\":\"124704\"},{\"game\":\"CxH\",\"platform\":\".com\",\"id\":\"29164\"},{\"game\":\"PSH\",\"platform\":\".com\",\"id\":\"1851\"}]},{\"name\":\"bla\",\"tier\":\"silver\",\"flairs\":[{\"game\":\"HH\",\"platform\":\"nutaku\",\"id\":\"2269930\"}]},{\"name\":\"Z-Wave\",\"tier\":\"bronze\"},{\"name\":\"Khole\",\"tier\":\"bronze\"}]');\n\n//# sourceURL=webpack://hh-plus-plus/./src/data/patrons.json?");
+eval("module.exports = JSON.parse('[{\"name\":\"Darkyz\",\"tier\":\"silver\",\"flairs\":[{\"game\":\"HH\",\"platform\":\".com\",\"id\":\"124704\"},{\"game\":\"CxH\",\"platform\":\".com\",\"id\":\"29164\"},{\"game\":\"PSH\",\"platform\":\".com\",\"id\":\"1851\"}]},{\"name\":\"sliqguy\",\"tier\":\"silver\",\"flairs\":[{\"game\":\"HH\",\"platform\":\"nutaku\",\"id\":\"977263\"}]}]');\n\n//# sourceURL=webpack://hh-plus-plus/./src/data/patrons.json?");
 
 /***/ })
 
